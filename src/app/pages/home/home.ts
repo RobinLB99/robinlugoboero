86c3a72e0e8a1c02f05f0129faed8fd558b5d476
Robin Lugo Boero <rjlb991102@hotmail.com>
2026-04-27 13:18:15 -0500

    feat(home): modernizar la sección Hero con iconos y acciones mejoradas

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRight, faComment } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public readonly faArrowRight = faArrowRight;
+  public readonly faComment = faComment;
+}
