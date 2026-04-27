a0590526d78b40ae07d3bad636b925c2fda47a2f
Robin Lugo Boero <rjlb991102@hotmail.com>
2026-04-27 13:18:00 -0500

    refactor(data): mejorar el modelo de proyectos con estructura de repositorio

--- src/app/services/project/project.service.ts
+++ src/app/services/project/project.service.ts
@@ -13,7 +13,10 @@ export class ProjectService {
         'Sistema Web para tiendas pequeñas que facilita el registro de ventas, control de caja, inventario y reportes en un solo lugar. ',
       tags: ['Spring Boot', 'Angular', 'Java', 'PostgreSQL', 'Tailwind CSS', 'Docker'],
       imageUrl: 'projects-preview/possystem.webp',
-      repoUrl: 'https://github.com/RobinLB99/point-of-sale',
+      repo: {
+        type: 'github',
+        url: 'https://github.com/RobinLB99/point-of-sale',
+      },
       status: 'En Desarrollo',
     },
     {
@@ -23,7 +26,10 @@ export class ProjectService {
         'API REST robusta y segura para gestión de servicios legales. Facilita la interacción entre abogados y clientes.',
       tags: ['Spring Boot', 'Java', 'PostgreSQL', 'Docker'],
       imageUrl: 'projects-preview/legalserviceportalapi.webp',
-      repoUrl: 'https://github.com/RobinLB99/legal-service-portal-api',
+      repo: {
+        type: 'github',
+        url: 'https://github.com/RobinLB99/legal-service-portal-api',
+      },
     },
     {
       name: 'DenunciasEcuador',
@@ -31,7 +37,10 @@ export class ProjectService {
       description: 'API Rest segura para la Gestión de denuncias.',
       tags: ['Spring Boot', 'Java', 'PostgreSQL', 'Docker'],
       imageUrl: 'projects-preview/denunciasecuadorapi.webp',
-      repoUrl: 'https://github.com/RobinLB99/denunciasecuador-api',
+      repo: {
+        type: 'github',
+        url: 'https://github.com/RobinLB99/denunciasecuador-api',
+      },
     },
 
     {
@@ -40,7 +49,10 @@ export class ProjectService {
       description: '3ra y actual versión de mi Portafolio personal.',
       tags: ['Angular', 'CSS Vanilla'],
       imageUrl: 'projects-preview/portafolio.webp',
-      repoUrl: 'https://github.com/RobinLB99/robinlugoboero',
+      repo: {
+        type: 'github',
+        url: 'https://github.com/RobinLB99/robinlugoboero',
+      },
     },
     {
       name: 'Portafolio v2',
@@ -48,7 +60,10 @@ export class ProjectService {
       description: '2da versión de mi Portafolio personal.',
       tags: ['Preact', 'Bootstrap'],
       imageUrl: 'projects-preview/portafolio-v2.webp',
-      repoUrl: 'https://github.com/RobinLB99/Portafolio_v2',
+      repo: {
+        type: 'github',
+        url: 'https://github.com/RobinLB99/Portafolio_v2',
+      },
       webUrl: 'https://robinlb99.github.io/Portafolio_v2',
     },
     {
@@ -57,7 +72,10 @@ export class ProjectService {
       description: 'Plataforma MVC para la Gestión de Tickets de Soporte Técnico.',
       tags: ['JSP', 'Java', 'JavaScript', 'Bootstrap', 'MariaDB', 'Docker'],
       imageUrl: 'projects-preview/techbit.webp',
-      repoUrl: 'https://github.com/RobinLB99/TECHBIT',
+      repo: {
+        type: 'github',
+        url: 'https://github.com/RobinLB99/TECHBIT',
+      },
     },
     {
       name: 'Librería',
@@ -66,7 +84,10 @@ export class ProjectService {
         'Plataforma MVC para la administración de una librería mediante la gestión de inventario, compra y venta de libros.',
       tags: ['JSP', 'Java', 'JavaScript', 'Bootstrap', 'MySQL'],
       imageUrl: 'projects-preview/libreria.webp',
-      repoUrl: 'https://github.com/RobinLB99/Tienda_de_libros',
+      repo: {
+        type: 'github',
+        url: 'https://github.com/RobinLB99/Tienda_de_libros',
+      },
     },
     {
       name: 'Tienda',
@@ -74,7 +95,10 @@ export class ProjectService {
       description: 'Software de escritorio para la gestión de productos de una tienda.',
       tags: ['Java', 'Java Swing', 'MySQL'],
       imageUrl: 'projects-preview/tienda.webp',
-      repoUrl: 'https://github.com/RobinLB99/tienda',
+      repo: {
+        type: 'github',
+        url: 'https://github.com/RobinLB99/tienda',
+      },
     },
   ];
 
