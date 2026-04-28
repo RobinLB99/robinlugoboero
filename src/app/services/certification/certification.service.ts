4c2c2a693ebd31928b3f15a110f0f002c9f7504c
Robin Lugo Boero <rjlb991102@hotmail.com>
2026-04-28 15:06:22 -0500

    feat(certification): agregar certificación de administrador de bases de datos

--- src/app/services/certification/certification.service.ts
+++ src/app/services/certification/certification.service.ts
@@ -7,6 +7,13 @@ import { Certificate } from '../../interfaces/certificate';
 export class CertificationService {
   private certifications: Certificate[] = [
     {
+      title: 'Administrador de bases de datos',
+      intitution: 'Capacítate para el Empleo',
+      dateExpetition: new Date(2026, 3, 27),
+      credentialID: 'C1B9AD2D-1A1A-416A-980A-804FB6676D80',
+      url: 'https://capacitateparaelempleo.org/verifica/eb86eb07-350a-4e42-aade-9ea92bc211aa/c1b9ad2d-1a1a-416a-980a-804fb6676d80',
+    },
+    {
       title: 'Curso profesional de Java',
       intitution: 'Código Facilito',
       dateExpetition: new Date(2024, 11, 22),
